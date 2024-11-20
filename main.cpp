7eb1e27f79790d21baf4408bf2a332ff9ea9d314
SmallDumplings <katyachecryglina@gmail.com>
2024-11-20 23:46:49 +0500

    +2 line in text

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ using namespace std;
 
 void get_text(ifstream& file, vector<string>& text_from_file){
     // функция которая считывает текст из файла
+    string s;
+    if(!file.is_open())
+    {
+        cout << "no open" << endl;
+        return;
+    }
+    while(getline(file, s)){
+        text_from_file.push_back(s);
+    }
 }
 
 void print_text(vector<string>& text_from_file){
@@ -21,6 +30,7 @@ int main()
 {
     vector<string> text_from_file;
     ifstream file("text.txt");
+
     get_text(file, text_from_file); //чтение текста из файла
     print_text(text_from_file); //вывод содержимого
     write_text(text_from_file);//записываю в файл
