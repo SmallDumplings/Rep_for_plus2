586c25f0a40c8da47a2603eb6523d1618252f683
SmallDumplings <katyachecryglina@gmail.com>
2024-11-21 00:44:09 +0500

    finich commit

--- main.cpp
+++ main.cpp
@@ -21,13 +21,22 @@ void get_text(ifstream& file, vector<string>& text_from_file){
 }
 
 void print_text(vector<string>& text_from_file){
+    // функция которая выводит текст из файла
     for (const auto& line : text_from_file){
         cout << line << endl;
-    }// функция которая выводит текст из файла
+    }
 }
 
 void write_text(vector<string>& text_from_file){
     // функция которая записывает текст из файла в другой файл
+    ofstream outputFile("text_output.txt", ios::binary);
+    stringstream ss;
+    for (const auto& line : text_from_file){
+        ss << line << endl;
+    }
+    string str = ss.str();
+    outputFile.write(str.c_str(), str.length());
+    outputFile.close();
 }
 
 int main()
