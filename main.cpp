1527c4245d8f1e1d8f3e82e078ebf70505dc7cec
krlrkv <ryzhankov.05@gmail.com>
2024-11-21 00:04:06 +0500

    изначально

--- main.cpp
+++ main.cpp
@@ -16,7 +16,14 @@ void print_text(vector<string>& text_from_file){
 }
 
 void write_text(vector<string>& text_from_file){
-    // функция которая записывает текст из файла в другой файл
+    ofstream outputFile("text_output.txt", ios::binary);
+    stringstream ss;
+    for (const auto& line : text_from_file) {
+        ss << line << endl;
+    }
+    string str = ss.str();
+    outputFile.write(str.c_str(), str.length());
+    outputFile.close();// функция которая записывает текст из файла в другой файл
 }
 
 int main()
